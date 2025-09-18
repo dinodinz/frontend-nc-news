402fefbc48c14c546cefbfa6df26e25a6994ac24
Dino <dinodungog@gmail.com>
2025-09-18 23:39:16 +0100

    use clearerrorpage reducer to reset errorpage value when loading homepage

--- src/components/UI/MainContent.jsx
+++ src/components/UI/MainContent.jsx
@@ -2,7 +2,7 @@ import AllArticles from "../Article/AllArticles";
 
 const MainContent = () => {
   return (
-    <div className="flex flex-col w-[95%] bg-[hsla(219,79%,41%,0.13)] rounded-[15px] mt-[20px] pt-[20px] mb-5[20px] relative">
+    <div className="flex flex-col w-[95%] bg-[hsla(219,79%,41%,0.13)] rounded-[15px] mt-[20px] pt-[20px] mb-[20px] relative">
       <AllArticles />
     </div>
   );
